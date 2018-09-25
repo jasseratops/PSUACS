b0008eeee2f94fc84a91b3be3574f14ddadc3406
jasseratops <mhalshehri92@gmail.com>
2018-09-25 00:03:10 -0500

    ACS502 HW3 Q5

--- ACS502/ACS502_HW3_3MedRefraction.py
+++ ACS502/ACS502_HW3_3MedRefraction.py
@@ -10,6 +10,43 @@ import matplotlib.pyplot as plt
 from numpy import pi, sin, cos, tan, exp
 
 
+def twoMed(rhoA,cA,rhoB,cB,theta_i):
+    zA = rhoA*cA
+    zB = rhoB*cB
+
+    theta_tr = np.arcsin((cB/cA)*sin(theta_i))
+    theta_cr = np.arcsin(cA/cB)
+
+    R = ((zB*cos(theta_i))-(zA*cos(theta_tr)))/((zB*cos(theta_i))+(zA*cos(theta_tr)))
+
+    return R, theta_cr
+
+def pMatGen(theta_i,Yacs1,Yacs2,Yacs3,k1l,k2l,k3l):
+    length = len(theta_i)
+    P = np.zeros(length,dtype=complex)
+
+
+    for i in range(length):
+        print i
+
+        M = np.array([[                       1.,                       1.,      -1.,                         0],
+                      [                 Yacs2[i],                -Yacs2[i], Yacs1[i],                         0],
+                      [          exp(-1j*k2l[i]),           exp(1j*k2l[i]),        0,          -exp(-1j*k3l[i])],
+                      [ exp(-1j*k2l[i])*Yacs2[i], -exp(1j*k2l[i])*Yacs2[i],        0, -exp(-1j*k3l[i])*Yacs3[i]]])
+        print M
+
+        c = np.array([1.,Yacs1[i],0.,0.])
+
+        invM = np.linalg.inv(M)
+
+        P[i] = np.matmul(invM,c)
+
+
+    print P
+
+    return P
+
+
 def main(args):
 
     rho1= 1025.
@@ -23,7 +60,6 @@ def main(args):
     rho3 = 1650.
     c3 = 1600.
     z3 = rho3 * c3
-
 
     '''
     #brewster
@@ -39,23 +75,23 @@ def main(args):
     c3 = 1500.
     Z3 = rho3 * c3
     '''
-    f= 1200.
-    omega = f*2*pi
+
 
     length = 1024
 
     theta_i = np.linspace(0.,pi/2.,length)
-    print np.degrees(theta_i)
     theta_gr = (pi/2.) - theta_i
-    print np.degrees(theta_gr)
     theta_t_13 = np.arcsin((c3/c1)*sin(theta_i))
 
 
+    R_13, theta_13_cr = twoMed(rhoA=rho1,cA=c1,rhoB=rho3,cB=c3,theta_i=theta_i)
 
-    R_13 = ((z3*cos(theta_i))-(z1*cos(theta_t_13)))/((z3*cos(theta_i))+(z1*cos(theta_t_13)))
 
-    theta_cr_13 = np.arcsin(c1/c3)
-    print "Theta_cr_13: " + str(np.degrees(theta_cr_13))
+    plt.figure()
+    plt.plot(np.degrees(theta_gr),abs(R_13))
+
+    plt.figure()
+    plt.plot(np.degrees(theta_gr),abs(R_13))
 
     theta_2 = np.arcsin((c2/c1)*sin(theta_i))
     theta_tr = np.arcsin((c3/c2)*sin(theta_2))
@@ -63,35 +99,18 @@ def main(args):
     Yacs2 = cos(theta_2)/z2
     Yacs3 = cos(theta_tr)/z3
 
-    kl = [0.2, 2., 20]
-    k1l = kl[0]
-    k2l = k1l*(sin(theta_i)/sin(theta_2))
-    k3l = k1l*(sin(theta_i)/sin(theta_tr))
-
-
-    for i in range(length):
-
-        M = np.array([[                 1.,                 1.,    -1.,                   0],
-                      [              Yacs2[i],             -Yacs2[i],  Yacs1[i],                   0],
-                      [       exp(-1j*k2l[i]),        exp(1j*k2l[i]),         0,       -exp(-1j*k3l[i])],
-                      [ exp(-1j*k2l)*Yacs2, -exp(1j*k2l)*Yacs2,      0, exp(-1j*k3l)*Yacs3]])
-
-        c = np.array([1.,Yacs1,0,0])
-
-    print "passed"
-
-    invM = np.linalg.inv(M)
-
-    P = np.matmul(invM,c)
-    print P
-
-    R = P[2]
+    kl = [0.2, 2., 20.]
 
     plt.figure()
-    plt.plot(np.degrees(theta_gr),abs(R_13))
+    for i in kl:
+        k1l = i
+        print k1l
+        k2l = k1l * (sin(theta_i) / sin(theta_2))
+        k3l = k1l * (sin(theta_i) / sin(theta_tr))
+        R3med = pMatGen(theta_i, Yacs1, Yacs2, Yacs3, i, k2l, k3l)[2,]
+        plt.plot(np.degrees(theta_gr),R3med,label="kl= " +str(i))
 
-    plt.figure()
-    plt.plot(np.degrees(theta_i),abs(R_13))
+    plt.legend()
     plt.show()
 
     return 0
