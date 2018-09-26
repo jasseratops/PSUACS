73dfeef6d864870668dd8bf75c8c9cbd7bede68e
jasseratops <mhalshehri92@gmail.com>
2018-09-26 12:51:54 -0500

    Jo Hays + Files

--- ACS597_SigAnalysis/ACS597_SigA_JoHays.py
+++ ACS597_SigAnalysis/ACS597_SigA_JoHays.py
@@ -6,6 +6,14 @@ import sounddevice as sd
 import soundfile as sf
 import sigA
 
+def indexGen(startTimeAn_EDT,startTimeRec_UTC,fs):
+    startTimeAn_UTC = startTimeAn_EDT + timedelta(hours=4)                          # convert from EDT to UTC
+    timeOffset = startTimeAn_UTC - startTimeRec_UTC                                 # Find time offset with relation to start of recording
+    print "timeOffset: " + str(timeOffset)
+    anStartInd = int(timeOffset.total_seconds()*fs)
+    print "Index at " + str(startTimeAn_EDT) + " EDT: " + str(anStartInd)
+    return anStartInd
+
 def main(args):
     #folder_name = "/Users/macbookpro/PycharmProjects/PSUACS/ACS597_SigAnalysis/Jo Hays/"
     #folder_name = "C:/Users/alshehrj/PycharmProjects/PSUACS/ACS597_SigAnalysis/"
@@ -18,24 +26,21 @@ def main(args):
     sampleRate =  wavFile.getframerate()
     noOfBits = wavFile.getsampwidth()*8*noOfSamples
 
+    cnvrtr = ((2**23)*(1.585E-6)*(1)*(1))/((1)*(1)*(50E-3)*200)
+
     print "sampleRate: " + str(sampleRate)
     print "noOfBits: " + str(noOfBits)
     print "noOfSamples: " + str(noOfSamples)
     startTimeAn_EDT = timedelta(days=1,hours=5,minutes=20)
-    startTimeAn_UTC = startTimeAn_EDT + timedelta(hours=4)                          # convert from EDT to UTC
-    startTimeRec_UTC = timedelta(hours=20,minutes=47,seconds=38,milliseconds=441)   # set recording start time
-    timeOffset = startTimeAn_UTC - startTimeRec_UTC                                 # Find time offset with relation to start of recording
-    print "timeOffset: " + str(timeOffset)
+    startTimeRec_UTC = timedelta(hours=20, minutes=47, seconds=38, milliseconds=441)  # set recording start time
 
-    anStartInd = int(timeOffset.total_seconds()*sampleRate)
-    print "Index at " + str(startTimeAn_EDT) + " EDT: " + str(anStartInd)
     nframes = int(10.*60.*sampleRate)
 
-
+    anStartInd = indexGen(startTimeAn_EDT, startTimeRec_UTC, fs=sampleRate)
 
     # wavFileread(path)[chunkStart:chunkEnd]
-    data = sf.read(file=path,frames=nframes,start=anStartInd)[0]
-    t = np.linspace(0,nframes,nframes)
+    data = (sf.read(file=path,frames=nframes,start=anStartInd)[0])*cnvrtr
+    t = np.linspace(0,nframes/sampleRate,nframes)
 
     print data
     print t
@@ -50,12 +55,24 @@ def main(args):
     GxxAvg, freqAvg, _, _ = sigA.spectroArray(data,fs=sampleRate,sliceLength=sliceLength,sync=0,overlap=0.5,winType="hann")
 
     plt.figure()
-    plt.semilogy(freqAvg,GxxAvg)
-    plt.xlim(0,400)
-    #plt.yscale("log")
+    plt.plot(t,data)
 
     plt.figure()
-    plt.plot(t,data)
+    plt.semilogy(freqAvg,GxxAvg)
+    plt.xlim(0,400)
+
+    specDatStartTime = timedelta(days=1,hours=5,minutes=26,seconds=0)
+    specDatInd = indexGen(startTimeAn_EDT=specDatStartTime,startTimeRec_UTC=startTimeRec_UTC,fs=sampleRate)
+    specDatNframes = 8*sampleRate
+
+    specDat = (sf.read(file=path,frames=specDatNframes,start=specDatInd)[0])*cnvrtr
+
+    plt.figure()
+    sigA.spectrogram(specDat,sampleRate,sliceLength=4096,overlap=0.5,winType="hann")
+
+    #plt.yscale("log")
+
+
     plt.show()
 
 if __name__ == '__main__':
