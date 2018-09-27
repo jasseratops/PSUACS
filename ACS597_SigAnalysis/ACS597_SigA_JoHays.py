64cca100ea4a3475c28c44cef4385bd1aba50d1b
jasseratops <mhalshehri92@gmail.com>
2018-09-26 20:34:14 -0500

    ACS597 Jo Hays Final

--- ACS597_SigAnalysis/ACS597_SigA_JoHays.py
+++ ACS597_SigAnalysis/ACS597_SigA_JoHays.py
@@ -2,7 +2,6 @@ import numpy as np
 import wave
 from datetime import date, time, datetime, timedelta
 import matplotlib.pyplot as plt
-import sounddevice as sd
 import soundfile as sf
 import sigA
 
@@ -15,18 +14,15 @@ def indexGen(startTimeAn_EDT,startTimeRec_UTC,fs):
     return anStartInd
 
 def main(args):
-    #folder_name = "/Users/macbookpro/PycharmProjects/PSUACS/ACS597_SigAnalysis/Jo Hays/"
-    #folder_name = "C:/Users/alshehrj/PycharmProjects/PSUACS/ACS597_SigAnalysis/"
-    folder_name = ""
-
     file_name = "AACD_208_20_47_38.wav"
-    path = folder_name+file_name
+    path = file_name
     wavFile = wave.open(path)
     noOfSamples = wavFile.getnframes()
     sampleRate =  wavFile.getframerate()
     noOfBits = wavFile.getsampwidth()*8
 
     cnvrtr = ((2**23)*(1.585E-6)*(1)*(1))/((1)*(1)*(50E-3)*200)
+    print cnvrtr
 
     print "sampleRate: " + str(sampleRate)
     print "noOfBits: " + str(noOfBits)
@@ -51,27 +47,40 @@ def main(args):
 
     sliceLength = 10*sampleRate
 
-    #sigA.spectrogram(data,sampleRate,sliceLength=sliceLength,overlap=0.5,scale=False,winType="hann")
+    sigA.spectrogram(data,sampleRate,sliceLength=1024,overlap=0.75,scale=False,winType="hann")
+    plt.title("Spectrogram: 5:20:00EDT-5:30:00EDT")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Frequency [Hz]")
+
     GxxAvg, freqAvg, _, _ = sigA.spectroArray(data,fs=sampleRate,sliceLength=sliceLength,sync=0,overlap=0.5,winType="hann")
 
     plt.figure()
     plt.plot(t,data)
+    plt.title("Waveform: 5:20:00EDT-5:30:00EDT")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude [Pa]")
+    plt.xlim(t[0],t[-1])
 
     plt.figure()
     plt.semilogy(freqAvg,GxxAvg)
+    plt.title("Spectral Density: 5:20:00EDT-5:30:00EDT")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Spectral Density [${Pa^2}$/ Hz]")
     plt.xlim(0,400)
 
-    specDatStartTime = timedelta(days=1,hours=5,minutes=26,seconds=0)
+    specDatStartTime = timedelta(days=1,hours=5,minutes=24,seconds=0)
     specDatInd = indexGen(startTimeAn_EDT=specDatStartTime,startTimeRec_UTC=startTimeRec_UTC,fs=sampleRate)
-    specDatNframes = 8*sampleRate
+    specDatNframes = int(10.*sampleRate)
 
     specDat = (sf.read(file=path,frames=specDatNframes,start=specDatInd)[0])*cnvrtr
+    print specDatNframes
 
     plt.figure()
-    sigA.spectrogram(specDat,sampleRate,sliceLength=4096,overlap=0.5,winType="hann")
-
-    #plt.yscale("log")
-
+    sigA.spectrogram(specDat,sampleRate,sliceLength=1024,overlap=0.5,winType="hann")
+    plt.xlim(0,8)
+    plt.title("Spectrogram: 5:24:00EDT - 5:24:08EDT")
+    plt.xlabel("time [s]")
+    plt.ylabel("Frequency [Hz]")
 
     plt.show()
 
