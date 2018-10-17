f9116ccfdce108c98be07baafa2eda592c907eec
jasseratops <mhalshehri92@gmail.com>
2018-10-17 12:15:29 -0500

    Jo Hays B

---
+++ ACS597_SigAnalysis/ACS597_SigA_MovingAvg.py
@@ -0,0 +1,24 @@
+# PSUACS
+# ACS597_SigA_MovingAvg
+# Jasser Alshehri
+# Starkey Hearing Technologies
+# 10/17/2018
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import pi, sin, cos, tan, exp
+import scipy.signal as sig
+import sigA
+
+def main(args):
+    fs = 1000.
+    T = 20
+    b = [1.,1.,1.,1.]
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main(sys.argv))
