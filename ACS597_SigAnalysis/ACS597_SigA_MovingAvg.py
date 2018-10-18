23e20b525d1d35828267d03374cd4c3d1fcaefcd
Jasser Alshehri <jasser_alshehri@starkey.com>
2018-10-17 19:22:03 -0500

    Jo Hays B

--- ACS597_SigAnalysis/ACS597_SigA_MovingAvg.py
+++ ACS597_SigAnalysis/ACS597_SigA_MovingAvg.py
@@ -13,8 +13,35 @@ import sigA
 
 def main(args):
     fs = 1000.
-    T = 20
-    b = [1.,1.,1.,1.]
+    T = 20.
+    N = int(fs*T)
+
+    delT,_,_ = sigA.param(N,fs)
+
+    Nc = 2
+    b = np.ones(Nc,dtype=float)*delT
+    impTrain = np.zeros(128)
+    impTrain[0] = 1./delT
+    impResp = sig.lfilter(b,1.,impTrain)
+
+    bdiff = np.array([1,-1])*delT
+    impRespDiff = sig.lfilter(bdiff,1.,impTrain)
+
+    w,h = sig.freqz(b,1)
+
+    print impResp
+    plt.figure()
+    plt.plot(impResp)
+
+    plt.figure()
+    plt.plot(impRespDiff)
+
+    plt.figure()
+    plt.plot(w,20*np.log10(np.abs(h)))
+    plt.show()
+
+
+
     return 0
 
 
