0df382a2a81e1b166e94247fa99b7d1f706a9ec3
Jasser Alshehri <jasser_alshehri@starkey.com>
2018-10-30 11:44:02 -0500

    ACS502 HW5

--- ACS502/ACS502_HW5_Q6_2.py
+++ ACS502/ACS502_HW5_Q6_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # PSUACS
 # ACS502_HW5_Q6
 # Jasser Alshehri
@@ -43,27 +42,3 @@ if __name__ == '__main__':
     import sys
 
     sys.exit(main(sys.argv))
-=======
-import numpy as np
-from numpy import log10, pi
-
-def main():
-    rho0 = 13600.
-    gamma = 1.13
-    c0 = 1450.
-    mu = 1.56
-    Cp = 0.14
-    kappa = 8.21
-    muB = 2.03
-
-    V_tilde = (4./3.)+(muB/mu)
-
-    Pr = mu*Cp/kappa
-
-    alpha_tv = (mu/(2*rho0*(c0**2)))*(V_tilde + ((gamma-1)/Pr))*(4*(pi**2))
-
-    print alpha_tv
-    return 0
-
-main()
->>>>>>> origin/master
