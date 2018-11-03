9eba725439497d5539ea9dc0d42af4a82f8604b8
jasseratops <mhalshehri92@gmail.com>
2018-11-02 20:55:46 -0500

    Quiz2

--- ACS597_SigAnalysis/ACS597_SigA_Quiz2.py
+++ ACS597_SigAnalysis/ACS597_SigA_Quiz2.py
@@ -25,22 +25,16 @@ def main(args):
 
     #part1(data_3BB, fs,ov,win,recLength)
     #part2(data_1BB,data_3BB,fs,ov,win,recLength)
+    #part3(data_3BB, fs, show=True)
     dataFiltered = part3(data_3BB,fs,show=False)
     part4(dataFiltered,fs)
     return 0
 
 def part1(data,fs,ov,win,recLength):
-    N = len(data)
-    times = sigA.timeVec(N,fs)
     t = 11.
     N11 = int(t*fs)
     frst11 = data[0:N11]
     scnd11 = data[N11:N11*2]
-
-    #msAvgd = sigA.
-
-
-
 
     Gxx_1,freq,delF,Gxx_1_array = sigA.spectroArray(frst11,fs,recLength,sync=0,overlap=ov,winType=win)
     Gxx_2,_,_,_ = sigA.spectroArray(scnd11,fs,recLength,sync=0,overlap=ov,winType=win)
@@ -52,22 +46,16 @@ def part1(data,fs,ov,win,recLength):
     rms_Gxx_Avg = sum(Gxx_1)*delF
     print "rms_Gxx_Avg: " + str(rms_Gxx_Avg)
 
-    ''' Gxx_tot Tangent
-    Nlong = int(m*recLength)
-    Gxx_tot = sigA.ssSpec(frst11[:Nlong],fs,winType=win)
-    _,delF_tot,_ = sigA.param(Nlong,fs,show=False)
-    rms_Gxx_tot = sum(Gxx_tot)*delF_tot
-    print "rms_Gxx_tot: " + str(rms_Gxx_tot)
-    '''
-
     plt.figure()
-    plt.semilogy(freq,Gxx_1)
-    plt.semilogy(freq,Gxx_2)
+    plt.semilogy(freq,Gxx_1,label="0-11s")
+    plt.semilogy(freq,Gxx_2,label="11-12s")
     plt.xlim(0,10E3)
     plt.ylim(1E-10,1E-3)
+    plt.title(r"$G_{xx}$")
     plt.xlabel("Frequency [Hz]")
-    plt.ylabel("Power Spectral Density [Pa^2/Hz]")
-
+    plt.ylabel(r"Power Spectral Density [${Pa^2}$/Hz]")
+    plt.legend()
+    plt.savefig("Part 1.png")
     plt.show()
 
     return 0
@@ -81,35 +69,6 @@ def part2(data1,data2,fs,ov,win,recLength):
     frst11_3bb = data2[:N11]
     scnd11_3bb = data2[N11:2*N11]
 
-    '''
-    S_XX =       sigA.dsSpec(frst11_1bb,fs,win)
-    S_XX_cross = sigA.crossSpec(frst11_1bb,frst11_1bb,fs,win)
-
-    print len(S_XX)
-    print len(S_XX_cross)
-
-    for i in range(len(S_XX)):
-        if S_XX[i]-S_XX_cross[i] > (1.E-19):
-            print i
-            print S_XX[i]
-            print S_XX_cross[i]
-            print S_XX[i]-S_XX_cross[i]
-            print 10*"-"
-            
-    plt.figure()
-    plt.plot(S_XX-S_XX_cross)
-    plt.figure()
-    plt.plot(S_XX)
-    #plt.plot(S_XX_cross)
-    
-    G_XX =       sigA.ssSpec(frst11_1bb,fs,win)
-    G_XX_cross = sigA.ssCrossSpec(frst11_1bb,frst11_1bb,fs,win)
-
-    plt.figure()
-    plt.plot(G_XX)
-    plt.plot(G_XX_cross)
-
-    '''
     frstCoh, freq = sigA.coherence(x_time= frst11_1bb,y_time=frst11_3bb,fs = fs,sliceLength=recLength,sync=0,overlap=ov,winType=win)
     scndCoh, _  = sigA.coherence(x_time=scnd11_1bb,y_time=scnd11_3bb,fs = fs,sliceLength=recLength,sync=0,overlap=ov,winType=win)
 
@@ -120,6 +79,8 @@ def part2(data1,data2,fs,ov,win,recLength):
     plt.title("Coherence")
     plt.xlabel("Frequency [Hz]")
     plt.ylabel(r"$\gamma^2$")
+    plt.legend()
+    plt.savefig("Part 2.png")
     plt.show()
 
     return 0
@@ -136,7 +97,7 @@ def part3(data1,fs,show):
     b,a = sig.butter(10,Wn,btype ="bandpass")
 
     w,h = sig.freqz(b,a)
-    theta = np.angle(h)
+    theta = np.degrees(np.angle(h))
     mag = 20*np.log10(abs(h))
     freqs = w*Nyq/(pi)
 
@@ -157,80 +118,103 @@ def part3(data1,fs,show):
         plt.figure()
         plt.subplot(211)
         plt.semilogx(freqs,mag)
+        plt.axvline(f1,color="black")
+        plt.axvline(f2,color="black")
+        plt.title("Butterworth BP [1kHz-6kHz], Magnitude")
+        plt.xlabel("Frequency [Hz]")
+        plt.ylabel("Magnitude")
         plt.subplot(212)
         plt.semilogx(freqs,theta)
-        plt.axvline(f1)
-        plt.axvline(f2)
+        plt.axvline(f1,color="black")
+        plt.axvline(f2,color="black")
+        plt.title("Butterworth BP [1kHz-6kHz], Phase")
+        plt.xlabel("Frequency [Hz]")
+        plt.ylabel("Phase [Degrees]")
+        plt.savefig("Part3, Butterworth BP.png")
+        plt.subplots_adjust(hspace=0.55)
 
         plt.figure()
         plt.plot(times,tot)
+        plt.xlim(3.630,3.640)
+        plt.title(r"sin(2$\pi$t(100Hz)) + sin(2$\pi$t(2kHz)) + sin(2$\pi$t(10kHz))")
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude [WU]")
+        plt.savefig("Part3, fuzzy sine.png")
 
         plt.figure()
-        plt.plot(times,totFilt)
-        plt.plot(times,sin2k)
-        plt.xlim(3.638,3.640)
+        plt.plot(times,totFilt, label= "Filtered Sine")
+        plt.plot(times,sin2k, label= "sin(2$\pi$t(2kHz))")
+        plt.xlim(3.630,3.640)
+        plt.title("Filtered sine")
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude [WU]")
+        plt.legend()
+        plt.savefig("Part3, filtered sine.png")
+
 
         plt.figure()
-        plt.plot(times,data1)
-        plt.plot(times,data1Filt)
+        plt.plot(times,data1,label = "Raw")
+        plt.plot(times,data1Filt, label = "Filtered")
         plt.title("N96_2017_3bb, raw vs filtered")
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude [Pa]")
+        plt.legend()
+        plt.savefig("Part 3, raw vs filtered.png")
         plt.show()
+
     return data1Filt
+
 
 def part4(data,fs):
     N = len(data)
-    times = sigA.timeVec(N,fs)
-    one_twelve = data[1*fs:12*fs]
+    times = sigA.timeVec(N,fs)[1*fs:12*fs]
+    one_twelve_3bb = data[1*fs:12*fs]
 
     recLength = int(1.*fs)
     Nrecs = 10
-    sampleShift = np.zeros(Nrecs)
 
-    data_avg,timeAvg,_,x_n = sigA.timeAvg(one_twelve,fs,recLength,Nrecs=Nrecs,sync=0)
-    _, tau = sigA.crossCorr(x_n[0,],x_n[-1,],fs)
+    slices = np.zeros((Nrecs,int(fs*1.)))
+
+    for i in range(Nrecs):
+        slices[i,]=one_twelve_3bb[i*recLength:(i+1)*recLength]
+        if i>0:
+            R_XY,tau = sigA.crossCorr(slices[0,],slices[i,],fs)
+            td = tau[np.argmax(R_XY)]
+            samd = int(td * fs)
+            slices[i,] = np.roll(slices[i],samd)
 
     plt.figure()
-    plt.plot(times,data)
-    plt.xlim(1,11)
+    for i in range(Nrecs):
+        plt.plot(times[:int(fs*1.)],slices[i,],label=str(i))
+        plt.legend()
+
+    slice_avg = np.mean(slices,axis=0)
+    x_n_avg = sigA.timeAvg(one_twelve_3bb,fs,recLength,Nrecs)[0]
 
     plt.figure()
-    plt.plot(timeAvg,x_n[8,])
-    plt.plot(timeAvg,data_avg)
-
-    for i in range(np.shape(x_n)[0]):
-        R = sigA.crossCorr(x_n[0,], x_n[i,], fs)[0]
-        td = tau[np.argmax(abs(R))]
-        sampleShift[i] = int(td*fs)
-        print "sample shift[" +str(i) + "]: " + str(sampleShift[i])
-    '''
-    plt.figure()
-    plt.plot(tau*fs,abs(R))
-    plt.axvline(td*fs)
-    '''
-    plt.figure()
-    slice = 4
-    plt.title("")
-    plt.plot(timeAvg,one_twelve[0:recLength],label="0")
-    plt.plot(timeAvg,one_twelve[int(slice * recLength):int((slice+1) * recLength)],label="9")
-    plt.plot(timeAvg,one_twelve[int((slice*recLength)-sampleShift[slice]):int(((slice+1)*recLength)-sampleShift[slice])],label="9+timeshift")
-
+    plt.plot(times[:int(fs*1.)],slices[0,],label="0-1")
+    plt.plot(times[:int(fs*1.)],x_n_avg,label="Avg")
+    plt.title("Single Record vs Time-Sync Averaging")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude [Pa]")
+    plt.xlim(times[0],times[fs])
     plt.legend()
-    plt.show()
-    '''
-    data_avg_sync,timeAvg_sync,_,x_n_sync = sigA.timeAvg(one_eleven,fs,recLength,Nrecs=Nrecs,sync=sampleShift)
+    plt.savefig("Part4.png")
 
-    for i in range(np.shape(x_n_sync)[0]):
-        R = sigA.crossCorr(x_n_sync[0,], x_n_sync[i,], fs)[0]
-        td = tau[np.argmax(abs(R))]
-        sampleShift[i] = int(td*fs)
-        print "sample shift[" +str(i) + "]: " + str(sampleShift[i])
 
     plt.figure()
-    plt.plot(timeAvg_sync, x_n_sync[8,])
-    plt.plot(timeAvg_sync, data_avg_sync)
+    plt.plot(times[:int(fs*1.)],slices[0,],label="0-1")
+    plt.plot(times[:int(fs*1.)],x_n_avg,label="avg")
+    plt.plot(times[:int(fs*1.)],slice_avg,label="Delay Corr")
+    plt.title("Single vs Time-Sync Avg'ing vs Time-Sync Avg'ing w/ Delay Correction")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude [Pa]")
+    plt.xlim(times[0],times[fs])
+    plt.legend()
+    plt.savefig("Part4, Improved.png")
 
     plt.show()
-    '''
+
 if __name__ == '__main__':
     import sys
 
