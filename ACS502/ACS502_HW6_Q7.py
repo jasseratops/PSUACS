fac4801dc72439807aacd7f2a4ee3e1523c61837
jasseratops <mhalshehri92@gmail.com>
2018-11-15 00:32:16 -0600

    ACS502 HW6

--- ACS502/ACS502_HW6_Q7.py
+++ ACS502/ACS502_HW6_Q7.py
@@ -1,10 +1,23 @@
 import numpy as np
 from numpy import sin, cos, tan, pi, exp, log10
 import sys
+import matplotlib.pyplot as plt
 
 
 def main(args):
+    kh = np.linspace(0.1,30,1024)
+    rat = (sin(2*kh)/(2*kh))+1
 
+    plt.figure()
+    plt.plot(kh,rat)
+    plt.axhline(1,linestyle="--")
+    for i in range(int(max(kh))):
+        plt.axvline(i*pi/2,linestyle=":")
+
+    plt.xlim(kh[0],kh[-1])
+    plt.xlabel("kh")
+    plt.ylabel(r"W/$W_{free}$")
+    plt.show()
     return 0
 
 
