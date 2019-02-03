9fb085e9e86c9c62d40e38287d1b04fce1069e2f
jasseratops <mhalshehri92@gmail.com>
2019-02-02 22:09:39 -0600

    515 HW3 final

--- ACS515/ACS515_HW3.py
+++ ACS515/ACS515_HW3.py
@@ -14,7 +14,7 @@ def main(args):
     N=1024
     kr = np.linspace(0.1,10.,N)
     i = -1j
-    u_r = (1./(kr**2))+(1j*(1./kr))
+    u_r = (1./(kr**2))-(i*(1./kr))
 
     for i in range(N):
         if np.real(u_r[i]) >= 0.99*np.abs(u_r[i]):
@@ -29,14 +29,15 @@ def main(args):
 
 
     plt.figure()
-    plt.semilogx(kr,np.abs(u_r))
-    plt.semilogx(kr,np.real(u_r))
-    plt.semilogx(kr,np.imag(u_r))
-    plt.axvline(near)
-    plt.axvline(far)
+    plt.semilogx(kr,np.abs(u_r),label=r"|${u_r}$|")
+    plt.semilogx(kr,np.real(u_r),label=r"Re{${u_r}$}")
+    plt.semilogx(kr,np.imag(u_r),label=r"Im{${u_r}$}")
+    plt.axvline(near, color="black")
+    plt.axvline(far, color="black")
     plt.xlabel("kr")
     plt.ylabel("Particle Velocity Amplitude [m/s]")
     plt.xlim(kr[0],kr[-1])
+    plt.legend()
     plt.show()
 
     return 0
