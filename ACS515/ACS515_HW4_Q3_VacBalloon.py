1ca9a371d62824d6b881b4d1d409b88fb3840877
Jasser Alshehri <jasser_alshehri@starkey.com>
2019-02-07 21:08:38 -0600

    515 HW4

--- ACS515/ACS515_HW4_Q3_VacBalloon.py
+++ ACS515/ACS515_HW4_Q3_VacBalloon.py
@@ -11,6 +11,50 @@ from numpy import pi, sin, cos, tan, exp
 
 
 def main(args):
+    a = 0.5
+    mu = 8435.
+    V = (4./3.)*pi*(a**3)
+    rho0 = 1.225
+    m_ball = 0.5
+    g = 9.81
+    S = 4*pi*(a**2)
+    c = 343.
+    kg_of_air = V*rho0
+    print kg_of_air
+
+    h = -mu*np.log(m_ball/(kg_of_air))
+    print h
+    rhoAir = rho0*np.exp(-h/mu)
+    s_eff = (g/mu)*V*rhoAir
+    Mrad = V*rhoAir*3.
+    M_eq = Mrad+m_ball
+
+    omega = np.sqrt(s_eff/M_eq)
+
+    print omega
+    f = omega/(2*pi)
+    print f
+    k = omega/c
+    ka = k*a
+    print "ka: " + str(ka)
+
+
+    R_rad = rhoAir*c*S*((ka**2)/(1+(ka**2)))
+    beta = R_rad/(2*M_eq)
+
+    tau = 1./beta
+    print tau
+    eta = 1.72E-5
+    R_visc = 6*pi*eta*a
+
+    gamma = R_visc/(2*M_eq)
+
+    beta_eq = beta+gamma
+    tau_eq = 1./beta_eq
+
+    print tau_eq
+
+    # http://hyperphysics.phy-astr.gsu.edu/hbase/oscda.html
     return 0
 
 
--- ACS515/ACs515_HW4_Q1_Bubbles.py
+++ ACS515/ACs515_HW4_Q1_Bubbles.py
@@ -8,17 +8,18 @@ def main(args):
     a = 1.E-3
     rho = 999.06
     # https://www.engineeringtoolbox.com/water-density-specific-weight-d_595.html
-    p0 = 100.E3
+    p0 = 1e5
     gamma = 1.4
     S=4.*pi*(a**2)
     c0 = 343.
     V = (4*pi*(a**3))/3.
+    m_rad = rho*S*a
+    s_gas = gamma*p0*(S**2)/V
+    f = (1./(2.*pi*a))*np.sqrt((3.*gamma*p0)/(rho))
+    f2 = (1/(2*pi))*np.sqrt(s_gas/m_rad)
 
-    m_a = 4.*pi*(a**3.)*3./(3.*(1.+(rho*(a**2.))/(gamma*p0)))
-    c_a = V/(gamma*p0)
-    f = (1./(2.*pi))*np.sqrt((3.*gamma*p0)/(rho*(a**2)))
-    f2= (1./(2.*pi))*np.sqrt(1./(c_a*m_a))
     T= 1./f
+
     print f
     print f2
     print T
@@ -28,13 +29,17 @@ def main(args):
     omega = 2*pi*f
     k = omega/c0
     ka = k*a
+    print "ka: " + str(ka)
 
     Rrad = rho*c0*S*((ka**2)/(1+(ka**2)))
-    beta = Rrad / 2*m_a
 
+    beta = Rrad/(2*m_rad)
 
+    tau = 1./beta
 
+    print tau
 
+    print tau/T
 
     return 0
 
