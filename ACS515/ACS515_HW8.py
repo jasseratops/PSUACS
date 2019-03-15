27f4ef712a0913576f5d6409aef8edfd8849ad6b
Jasser Alshehri <jasser_alshehri@starkey.com>
2019-03-14 19:42:01 -0500

    515 HW8

--- ACS515/ACS515_HW8.py
+++ ACS515/ACS515_HW8.py
@@ -9,43 +9,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import pi, sin, cos, tan, exp
 from mpl_toolkits import mplot3d
+from matplotlib import cm
+
 
 
 
 def main(args):
     N = 1024
-    L_x = 400.
-    L_y = 400.
+    L_x = 1.
+    L_y = 1.
 
-    #x = np.linspace(-L_x/2.,L_x/2.,N)
-    #y = np.linspace(-L_y/2.,L_y/2.,N)
+    x = np.linspace(-L_x/2.,L_x/2.,N)
+    y = np.linspace(-L_y/2.,L_y/2.,N)
 
-    x = np.linspace(0,3*L_x,N)
-    y = np.linspace(0,3*L_y, N)
+    #lim = np.max([L_x,L_y])/2.
+    lim = 50.
 
     X, Y = np.meshgrid(x,y)
 
-    Z = u_n(X/L_x,Y/L_y)
+    Z = u_n(X,Y,L_x,L_y)
     print np.shape(x/L_x)
     print np.shape(Z)
 
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    ax.contour3D(X, Y, Z, 50, cmap='binary')
+    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm)
+    plt.xlim(lim*-1.1,lim*1.1)
+    plt.ylim(lim*-1.1,lim*1.1)
+
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    ax.set_zlabel('z');
+    ax.set_zlabel('z')
 
     plt.show()
 
     return 0
 
-def u_n(x_s,y_s):
-    return exp(-1*(x_s+y_s))
+def u_n(x_s,y_s,L_x,L_y):
+    return exp(-1*(((x_s/L_x)**2)+((y_s/L_y)**2)))
 
 
 
 if __name__ == '__main__':
     import sys
-
     sys.exit(main(sys.argv))
