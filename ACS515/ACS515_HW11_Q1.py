b6f8b59ff56d2befe73005de8467d0e21c731b3d
Jasser Alshehri <jasser_alshehri@starkey.com>
2019-04-05 17:41:55 -0500

    515 HW11

--- ACS515/ACS515_HW11_Q1.py
+++ ACS515/ACS515_HW11_Q1.py
@@ -4,7 +4,6 @@
 # Starkey Hearing Technologies
 # 4/3/2019
 
-from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import pi, sin, cos, tan, exp
@@ -25,22 +24,46 @@ def main(args):
     ka = k*a
     kr = k*r
 
+    print "ka: " + str(ka)
+    print "kr: " + str(kr)
+    print 10*"-"
+
     p_pk = p_ref*10**(SPL/20.)
+    u0_rad_approx = (p_pk/(rho*c))*np.sqrt(r/a)*np.sqrt(2./(pi*ka))
+    u0_rad_comp = (p_pk/(rho*c))*np.sqrt(pi/2.)*np.sqrt(kr)*np.sqrt(((ka/2.)**2)+((2./(pi*ka))**2))
+    xi0_rad_approx = u0_rad_approx/omega
 
-    debug('kr')
-    debug('ka')
+    print "Radial"
+    print "Vel. (Approx.): "+str(u0_rad_approx)
+    print "Vel. (Comp.): " +str(u0_rad_comp)
+    print 10*"-"
+    Hder = 0.5*(0.5*(2.-(ka/2)**2)-1j*(1./pi)*((2*(np.log(kr)-0.116))+(2./ka)**2))
+    HderMag = np.abs(Hder)
 
+
+    u0_trans_approx = (p_pk/(rho*c))*(1./ka)*np.sqrt(r/a)*np.sqrt(2./(pi*ka))
+    u0_trans_comp = (p_pk/(rho*c))*np.sqrt(pi*kr/2.)*HderMag
+    xi0_trans_approx = u0_trans_approx/omega
+
+    print "Trans."
+    print "Vel. (Approx.): "+str(u0_trans_approx)
+    print "Vel. (Comp.): " +str(u0_trans_comp)
+    print 10*"-"
+
+    print "Displacement"
+    print "Radial: " + str(xi0_rad_approx)
+    print "Trans.: " + str(xi0_trans_approx)
 
 
     return 0
-
+'''
 def debug(expression):
     frame = sys._getframe(1)
     result = (expression, ': ', repr(eval(expression, frame.f_globals, frame.f_locals)))
     for i in result:
         print(i,end ="")
     return 0
-
+'''
 
 if __name__ == '__main__':
     import sys
