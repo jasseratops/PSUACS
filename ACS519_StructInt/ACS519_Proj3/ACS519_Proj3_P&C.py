e551de1c0e32a4f4ee3cf6fe5f8616fbadec32e0
jasseratops <mhalshehri92@gmail.com>
2019-12-08 15:38:48 -0600

    ACS519 Final Project Init

---
+++ ACS519_StructInt/ACS519_Proj3/ACS519_Proj3_P&C.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy import sin, cos, tan, pi, exp, log10
+import sys
+import matplotlib.pyplot as plt
+import ACS519_StructInt.soundStructureInt as ssint
+
+
+def main(args):
+    return 0
+
+def part1():
+    return 0
+
+def part2():
+    return 0
+
+def third_oct_band(f_start,f_stop):
+    f_points=0
+    return f_points
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
