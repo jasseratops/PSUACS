6326920eec922cadb13f0fe0ff0d658a93135e9a
jasseratops <mhalshehri92@gmail.com>
2019-12-15 18:30:10 -0600

    ACS519 Final Project: * Complete, needs review

--- ACS519_StructInt/ACS519_Proj3/ACS519_Proj3_P&C.py
+++ ACS519_StructInt/ACS519_Proj3/ACS519_Proj3_P&C.py
@@ -8,20 +8,144 @@ import ACS519_StructInt.soundStructInt as ssint
 
 
 def main(args):
-    # eta_p,E_p,v_p,rho_p,l2_p1,l2_p2,c_cav,rho_cav
-    part1()
+    f = third_oct_band(20,10000,center=True)
+    f = np.append(f,10**4)
+    save = True
+    part1(f,save)
+    part2(f,save)
+    part3(f,save)
+    plt.show()
+
+def part1(f,save=False):
+
+    # Figure 5
+    TL_Fig5, ML_Fig5 =RPCPR(f=f, eta_p=0.005, E_p=69.1E9, v_p=0.3, rho_p=2673., l2_p1=3.18E-3, l2_p2=3.18E-3, c_cav=343., rho_cav=1.29)
+    # Figure 6
+    TL_Fig6, ML_Fig6 = RPCPR(f=f, eta_p=0.005, E_p=69.1E9, v_p=0.3, rho_p=2673., l2_p1=2 * 3.18E-3, l2_p2=3.18E-3, c_cav=343., rho_cav=1.29)
+    # Figure 8
+    TL_Fig8, ML_Fig8 = RPCPR(f=f, eta_p=0.05, E_p=69.1E9, v_p=0.3, rho_p=2673., l2_p1=3.18E-3, l2_p2=3.18E-3, c_cav=343., rho_cav=1.29)
+
+    plt.figure()
+    plt.title("TL (P&C Fig.5)")
+    plt.semilogx(f,TL_Fig5,label=r"TL, ${\eta_p}$ = 0.005",color = "red")
+    plt.semilogx(f,ML_Fig5,label= "Mass Law",color="black", linestyle=(0, (5, 5)))
+    plt.xlim(100,10000)
+    plt.ylim(0,75)
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Transmission Loss [dB]")
+    plt.grid(which="both")
+    plt.legend()
+    if save: plt.savefig("Fig5.png")
+
+    plt.figure()
+    plt.title("TL, double panel 1 thickness, (P&C Fig.6)")
+    plt.semilogx(f,TL_Fig6,label=r"TL, ${\eta_p}$ = 0.005",color = "red")
+    plt.semilogx(f,ML_Fig6,label= "Mass Law",color="black", linestyle=(0, (5, 5)))
+    plt.xlim(100,10000)
+    plt.ylim(0,75)
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Transmission Loss [dB]")
+    plt.grid(which="both")
+    plt.legend()
+    if save: plt.savefig("Fig6.png")
+
+
+    plt.figure()
+    plt.title("TL (P&C Fig.5)")
+    plt.semilogx(f,TL_Fig5,label=r"TL, ${\eta_p}$ = 0.005",color = "red")
+    plt.semilogx(f,ML_Fig5,label= "Mass Law",color="black", linestyle=(0, (5, 5)))
+    plt.semilogx(f,TL_Fig8,label=r"TL, ${\eta_p}$ = 0.05",color = "green",linestyle="-.")
+    plt.xlim(100,10000)
+    plt.ylim(0,75)
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Transmission Loss [dB]")
+    plt.grid(which="both")
+    plt.legend()
+    if save: plt.savefig("Fig8.png")
+
     return 0
 
-def part1():
+def part2(f,save=False):
+
+    a = np.array([[5,5],[5,10],[6,10],[10,10]])*1.E-3
+
+    plt.figure()
+    plt.title("Transmission Loss, Glass Panels")
+
+    for i in range(len(a)):
+        l2_p1 = a[i][0]
+        l2_p2 = a[i][1]
+        lab = r"TL, l2_p1="+str(int(l2_p1*1E3))+"mm, l2_p2="+str(int(l2_p2*1E3))+"mm"
+        TL_glass, ML_glass = RPCPR(f=f, eta_p=0.01, E_p=61.3E9, v_p=0.2, rho_p=2100., l2_p1=l2_p1, l2_p2=l2_p2,
+                                   c_cav=343.,rho_cav=1.29)
+        p = plt.semilogx(f, TL_glass, label = lab)
+        plt.semilogx(f, ML_glass, linestyle=(0, (5, 5)), color=p[0].get_color())
+
+    plt.axvline(1E3,color="black",linestyle="--")
+    plt.axvline(6E3,color="black",linestyle="--")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Transmission Loss [dB]")
+    plt.xlim(100,10000)
+    plt.ylim(0,120)
+    plt.grid(which="both")
+    plt.legend()
+    if save: plt.savefig("Part 2: Glass Panels")
+
+    return 0
+
+def part3(f,save=False):
+    ### from https://www.engineeringtoolbox.com/speed-sound-gases-d_1160.html
+    ### and https://pages.mtu.edu/~suits/SpeedofSoundOther.html
+    c_air = 343.
+    c_He = 1007.
+    c_Ne = 461.
+    c_Ar = 323.
+    c_Kr = 224.
+    c_Xe = 178.
+
+    rho_air = 1.29
+    rho_He = 0.17847
+    rho_Ne = 0.899
+    rho_Ar = 1.784
+    rho_Kr = 3.75
+    rho_Xe = 5.881
+
+    c_array = np.array([c_air,c_He,c_Ne,c_Ar,c_Kr,c_Xe])
+    rho_array = np.array([rho_air,rho_He,rho_Ne,rho_Ar,rho_Kr,rho_Xe])
+    str_array = ["air","He","Ne","Ar","Kr","Xe"]
+
+    plt.figure()
+    plt.title("Transmission Loss, Inert Gases")
+    for i in range(len(c_array)):
+        c_cav = c_array[i]
+        rho_cav = rho_array[i]
+        TL_glass, ML_glass = RPCPR(f=f, eta_p=0.01, E_p=61.3E9, v_p=0.2, rho_p=2100., l2_p1=10E-3, l2_p2=5E-3,
+                                   c_cav=c_cav, rho_cav=rho_cav)
+
+        p = plt.semilogx(f, TL_glass, label=str_array[i])
+        plt.semilogx(f, ML_glass, linestyle=(0, (5, 5)), color=p[0].get_color())
+
+    plt.axvline(1E3,color="black",linestyle="--")
+    plt.axvline(6E3,color="black",linestyle="--")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Transmission Loss [dB]")
+    plt.xlim(100,10000)
+    plt.ylim(0,120)
+    plt.grid(which="both")
+    plt.legend()
+    if save: plt.savefig("Part 3: Inert Gases")
+
+    return 0
+
+
+def RPCPR(f, eta_p, E_p, v_p, rho_p, l2_p1, l2_p2, c_cav, rho_cav):
     eta = loss_factor()
     n = modal_density()
     tl = transmission_loss()
 
     c_0= 343.
-    c_cav=c_0
     rho_0 = 1.29
-    f = third_oct_band(20,10000,center=True)
-    f = np.append(f,10**4)
+
     omega = 2*pi*f
 
     alpha_0 = absorption_coefficient(f)
@@ -41,23 +165,16 @@ def part1():
     eta_5 = 0.001*np.ones_like(f)
 
     ### Panel loss factors
-    eta_2 = 0.005*np.ones_like(f)
-    eta_4 = 0.005*np.ones_like(f)
+    eta_2 = eta_p*np.ones_like(f)
+    eta_4 = eta_p*np.ones_like(f)
 
-
-    ### Material Properties for Aluminum (6061-T6)
-    E_p = 69.1E9  # Pa
-    v_p = 0.3  #
-    rho_p = 2673.  # kg/m^3
 
     ### Panel 1 dimensions
     l1_p1 = l1_c         # Panel Width
-    l2_p1 = 3.18E-3      # Panel Thickness
     l3_p1 = l3_c         # Panel Height
 
     ### Panel 2 dimensions
     l1_p2 = l1_c         # Panel Width
-    l2_p2 = 3.18E-3      # Panel Thickness
     l3_p2 = l3_c         # Panel Height
 
     M_p1 = l1_p1*l2_p1*l3_p1*rho_p
@@ -66,14 +183,14 @@ def part1():
     c_p1, _, _ = ssint.plate_phase_speed(l2_p1, v_p, E_p, rho_p, omega, K=5.6, eta=eta_2)
     c_p2, _, _ = ssint.plate_phase_speed(l2_p2, v_p, E_p, rho_p, omega, K=5.6, eta=eta_4)
 
-    omega_c_p1 = ssint.critical_frq_panel(c_B=c_p1,c_0=c_0,omega=omega)
-    omega_c_p2 = ssint.critical_frq_panel(c_B=c_p2,c_0=c_0,omega=omega)
+    f_c_p1_room = ssint.critical_frq_panel(c_B=c_p1,c_0=c_0,omega=omega)/(2.*pi)
+    f_c_p2_room = ssint.critical_frq_panel(c_B=c_p2,c_0=c_0,omega=omega)/(2.*pi)
 
-    f_c_p1 = omega_c_p1/(2.*pi)
-    f_c_p2 = omega_c_p2/(2.*pi)
+    f_c_p1_cav  = ssint.critical_frq_panel(c_B=c_p1,c_0=c_cav,omega=omega)/(2.*pi)
+    f_c_p2_cav  = ssint.critical_frq_panel(c_B=c_p2,c_0=c_cav,omega=omega)/(2.*pi)
 
-    print "Critical Frq (panel 1): " + str(f_c_p1)
-    print "Critical Frq (panel 2): " + str(f_c_p2)
+    print "Critical Frq (panel 1): " + str(f_c_p1_room)
+    print "Critical Frq (panel 2): " + str(f_c_p2_room)
 
     alpha_0 = absorption_coefficient(f)
 
@@ -81,17 +198,20 @@ def part1():
     eta_3 = eta.cavity(f,c_cav,l1_c,l2_c,l3_c,alpha_0)
 
     ### Panel radiation efficiency
-    R_rad_p1,rad_eff_p1 = halfSpace_radRes(f,f_c_p1,l1_p1,l3_p1,rho_0,c_0)
-    R_rad_p2,rad_eff_p2 = halfSpace_radRes(f,f_c_p2,l1_p2,l3_p2,rho_0,c_0)
+    R_rad_p1_room,rad_eff_p1_room = halfSpace_radRes(f,f_c_p1_room,l1_p1,l3_p1,rho_0,c_0)
+    R_rad_p2_room,rad_eff_p2_room = halfSpace_radRes(f,f_c_p2_room,l1_p2,l3_p2,rho_0,c_0)
 
+    R_rad_p1_cav,rad_eff_p1_cav  = halfSpace_radRes(f,f_c_p1_cav,l1_p1,l3_p1,rho_cav,c_cav)
+    R_rad_p2_cav,rad_eff_p2_cav  = halfSpace_radRes(f,f_c_p2_cav,l1_p2,l3_p2,rho_cav,c_cav)
 
     ### Model Densities
     n1 = n.room(f,l1_r,l2_r,l3_r,c_0)
     n2 = n.panel(f,c_p1,l1_p1,l3_p1)
-    n3 = n.cavity(f,c_0,l1_c,l3_c,l2_c)
+    n3 = n.cavity(f,c_cav,l1_c,l3_c,l2_c)
     n3_fake = n.room(f,l1_c,l3_c,l2_c,c_cav,cavity=True)
     n4 = n.panel(f,c_p2,l1_p2,l3_p2)
     n5 = n.room(f,l1_r,l2_r,l3_r,c_0)
+
 
     TL_13 = tl.rand_inc_TL(f,rho_p,l2_p1,rho_0,c_0)
     TL_53 = tl.rand_inc_TL(f,rho_p,l2_p2,rho_0,c_0)
@@ -100,10 +220,10 @@ def part1():
     V1 = l1_r*l2_r*l3_r
 
     ### Loss factors
-    eta_21 = eta.panel_to_room(f,R_rad_p1,M_p1,f_c_p1)
-    eta_23 = eta.panel_to_cav(f, R_rad_p1,M_p1,f_c_p1)
-    eta_45 = eta.panel_to_room(f,R_rad_p2,M_p2,f_c_p2)
-    eta_43 = eta.panel_to_cav(f, R_rad_p2,M_p2,f_c_p2)
+    eta_21 = eta.panel_to_room(f,R_rad_p1_room,M_p1,f_c_p1_room)
+    eta_23 = eta.panel_to_cav(f, R_rad_p1_cav,M_p1,f_c_p1_cav)
+    eta_45 = eta.panel_to_room(f,R_rad_p2_room,M_p2,f_c_p2_room)
+    eta_43 = eta.panel_to_cav(f, R_rad_p2_cav,M_p2,f_c_p2_cav)
     eta_13 = eta.room_to_cav(f,TL_13,A2,V1,c_0)
     eta_53 = eta.room_to_cav(f,TL_53,A2,V1,c_0)
 
@@ -133,6 +253,11 @@ def part1():
     eta_44 = eta_4 + (eta_41 + eta_42 + eta_43 + eta_45)
     eta_55 = eta_5 + (eta_51 + eta_52 + eta_53 + eta_54)
 
+    massLaw_p1 = tl.rand_inc_TL(f,rho_p,l2_p1,rho_0,c_0)
+    massLaw_p2 = tl.rand_inc_TL(f,rho_p,l2_p2,rho_0,c_0)
+
+    massLaw_dB = massLaw_p1 + massLaw_p2
+
 
     TL_15 = np.zeros_like(f)
     for i in range(len(f)):
@@ -147,10 +272,30 @@ def part1():
         E_mtx = lin.solve(eta_mtx,P_mtx)
         E_1 = E_mtx[0]
         E_5 = E_mtx[-1]
-
+        #print E_1
+        #print str(i) + ": " + str(E_5)
         TL_15[i] = 10.*log10(E_1/E_5)
 
-
+    eta_1i = [eta_12, eta_13, eta_14, eta_15]
+    eta_2i = [eta_21,eta_23,eta_24,eta_25]
+    eta_3i = [eta_31, eta_32, eta_34, eta_35]
+    eta_4i = [eta_41,eta_42,eta_43,eta_45]
+    eta_5i = [eta_51,eta_52,eta_53,eta_54]
+    '''
+    plt.figure()
+    for i in range(4):
+        plt.plot(f,eta_1i[i][:])
+    '''
+    '''
+    plt.figure()
+    plt.plot(f,)
+    plt.figure()
+    plt.plot(f,)
+    plt.figure()
+    plt.plot(f,)
+    plt.figure()
+    plt.plot(f,)
+    '''
 
     '''
     plt.figure()
@@ -158,50 +303,34 @@ def part1():
     plt.xlim(10**2,10**5)
     plt.ylim(10**-3,10**-1)
     plt.grid(which="both")
-    '''
+
     plt.figure()
-    plt.loglog(f,rad_eff_p1)
+    plt.loglog(f,rad_eff_p1_room)
     plt.xlim(100.,10000.)
     plt.ylim(1.E-3,1.E1)
     plt.grid(which="both")
 
-    '''
+
     plt.figure()
+    plt.title("cavity modal density")
     plt.loglog(f,n3,linestyle="-")
     plt.loglog(f,n3_fake)
     plt.xlim(10**2,10**5)
     plt.ylim(1.E-2,1.E0)
     plt.grid(which="both")
-    '''
-    '''
-    plt.figure()
-    plt.semilogx(f*rho_Al*l2_panel,TL_13)
-    plt.xlim(10,10**6)
-    plt.grid(which="both")
-    '''
+
 
 
     plt.figure()
-    plt.semilogx(f,TL_15)
-    plt.xlim(100,10000)
-    plt.ylim(0,75)
-    plt.grid(which="both")
-    '''
-    plt.figure()
+    plt.title("Room to Panel loss factor")
     plt.loglog(f,eta_12)
     plt.loglog(f,eta_21)
-    '''
     plt.ylim(1E-7,1E-1)
     plt.xlim(100,10000)
     plt.grid(which="both")
+    '''
 
-    plt.show()
-
-    return 0
-
-def part2():
-    return 0
-
+    return TL_15, massLaw_dB
 
 def third_oct_band(f_start,f_stop,center=True):
     f_ref = 1000.
